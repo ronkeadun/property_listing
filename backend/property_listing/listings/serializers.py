cfac54f6c1e26e6b229c624b27e8835defeb3cd4
ronkeadun <ronkeadun1@yahoo.com>
2020-08-30 00:15:17 +0100

    feature(App):add listings app [Finishes #7]

---
+++ backend/property_listing/listings/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Listing
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ('title','address','city','state','price','sale_type','home_type','bedrooms','bathrooms','sqft','photo_main','slug')
+
+class ListingDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        #fields = '__all__'
+        fields = ('title','address','city','state','price','sale_type','home_type','bedrooms','bathrooms','sqft','photo_main','slug')
+        lookup_field = 'slug'
--- backend/property_listing/property_listing/settings.py
+++ backend/property_listing/property_listing/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'realtors',
+    'listings',
 ]
 
 MIDDLEWARE = [
