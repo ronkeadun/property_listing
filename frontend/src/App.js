9c86dee93bba26160324d35f0da321280055f866
ronkeadun <ronkeadun1@yahoo.com>
2020-09-02 21:33:20 +0100

    feature(Auth): implement token-based authentication with React using JSON Web Token (JWT) [Finishes #10]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,23 +10,27 @@ import SignUp from "./containers/SignUp";
 import NotFound from "./components/NotFound";
 import Layout from "./hocs/Layout";
 import "./sass/main.scss";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/listings" component={Listings} />
-        <Route exact path="/listings/:id" component={ListingDetail} />
-        <Route exact path="/login" component={LogIn} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-    ;
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/listings" component={Listings} />
+          <Route exact path="/listings/:id" component={ListingDetail} />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+      ;
+    </Router>
+  </Provider>
 );
 
 export default App;
