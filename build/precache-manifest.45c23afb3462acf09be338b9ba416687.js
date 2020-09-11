a75eca65fef0273258c7a9e5e1487947a7419e81
ronkeadun <ronkeadun1@yahoo.com>
2020-09-11 09:57:28 +0100

    chore(development):add additional dependencies

---
+++ build/precache-manifest.45c23afb3462acf09be338b9ba416687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce704fb54dcd3d624fff5d90585bdbc",
+    "url": "/ronkeadun/property_listingindex.html"
+  },
+  {
+    "revision": "8ba8704637e9dcd230ca",
+    "url": "/ronkeadun/property_listingstatic/css/main.dd69f8b5.chunk.css"
+  },
+  {
+    "revision": "cdfab6fb0b78ee8309f8",
+    "url": "/ronkeadun/property_listingstatic/js/2.b5f433b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ronkeadun/property_listingstatic/js/2.b5f433b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba8704637e9dcd230ca",
+    "url": "/ronkeadun/property_listingstatic/js/main.26c05b85.chunk.js"
+  },
+  {
+    "revision": "e963fd21bacf350c42fd",
+    "url": "/ronkeadun/property_listingstatic/js/runtime-main.8cbb778a.js"
+  },
+  {
+    "revision": "54ad755d0ac2578303b874b92a394958",
+    "url": "/ronkeadun/property_listingstatic/media/house.54ad755d.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a6789351722378bc172b4a6ee2490f2.js"
+  "/ronkeadun/property_listingprecache-manifest.45c23afb3462acf09be338b9ba416687.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ronkeadun/property_listingindex.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
